2f959b2c0465175f69d31a3ae222f006585d9872
orlando manfredini <orlando.manfre1@gmail.com>
2024-03-12 14:23:26 +0100

    ADD: primo snack (non ci sono arrivato da solo al ciclo fo ma ho capito come legge le parole

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-console.log('ciao')
+
 
 
 // --- consegna 1
@@ -13,13 +13,31 @@ console.log('ciao')
 // chiedo a utente di inserire parola con promt
 
 const wordUser = prompt('Inserisci parola')
-console.log(wordUser)
+// console.log(wordUser)
+
+let wordReverse = '';
+for(let i = wordUser.length -1 ; i >= 0; i--){
+    wordReverse += wordUser[i]
+
+}
+// console.log(wordReverse)
+
+let relust = isPalindrome(wordUser, wordReverse)
 
 // creo una funzione per leggere la parola inserita
 
-function isPalindrome(){
+function isPalindrome(word1, word2){
 
-
+    if(word1 === word2){
+        //console.log('Palindromo')
+        return true
+    }else{
+        //console.log('Non Palindromo')
+        return false
+    }
 
     // output true o false
 }
+
+console.log(relust)
+
