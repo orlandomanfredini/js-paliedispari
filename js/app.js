10e107dde81657febc49ada7409c69cb88ef689f
orlando manfredini <orlando.manfre1@gmail.com>
2024-03-12 13:48:26 +0100

    ADD: inizio consegna  Palindromi

---
+++ js/app.js
@@ -0,0 +1,25 @@
+console.log('ciao')
+
+
+// --- consegna 1
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma:
+// la funzione deve ritornare true se la parola è palindroma
+// deve ritornare false se la parola non è palindroma
+// alla fine stampate in console un messaggio per dire 
+// all’utente il risultato del controllo
+
+// chiedo a utente di inserire parola con promt
+
+const wordUser = prompt('Inserisci parola')
+console.log(wordUser)
+
+// creo una funzione per leggere la parola inserita
+
+function isPalindrome(){
+
+
+
+    // output true o false
+}
