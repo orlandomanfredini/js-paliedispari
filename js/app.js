f1ab40e507c4ab0e49a449e3cdc47fe074d32a2d
orlando manfredini <orlando.manfre1@gmail.com>
2024-03-12 17:16:03 +0100

    ADD: aggiunta controllo (suggerito)

--- js/app.js
+++ js/app.js
@@ -1,4 +1,23 @@
 
+ let scelta;
+
+// true && false => false
+// false && false => false
+// true && true => true
+// true || false => true
+// true || true => true
+// (false || false) => false
+// !(false || false) => !(false) => true
+
+
+ do {
+     scelta = prompt('inserisci "pari" o "dispari"'); // ciao
+ } while (!(scelta === 'pari' || scelta === 'dispari')); // !(false || false) => !(false) => true
+
+ console.log(scelta); // pari o dispari
+
+
+
 
 
 // --- consegna 1
@@ -24,7 +43,57 @@ for(let i = wordUser.length -1 ; i >= 0; i--){
 
 let relust = isPalindrome(wordUser, wordReverse)
 
-// creo una funzione per leggere la parola inserita
+console.log(relust)
+
+// --- consegna 2
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+
+
+let whoWin = '';
+do{
+    // chiedo a utente se il risultato della somma è pari o dispari
+     whoWin = prompt('il risultato della somma è pari o dispari?');//string
+    
+}while(!(whoWin === 'pari' || whoWin === 'dispari'))
+
+console.log('utente ha scelto ' + whoWin);
+// chiesto a utente di inserire numero da 1 a 5 
+let numberUser = parseInt(prompt('inserisci numero da 1 a 5')); //number
+console.log(numberUser); //number
+
+// genero un numero random dal computer 
+let randomNumber = Math.floor(Math.random()* 5) + 1; //number
+console.log(randomNumber); //number
+
+
+
+
+
+let finalResults = sumNumber(numberUser, randomNumber); // invocazione functio isSumEveen
+console.log('la somma è ' + finalResults)
+
+
+
+
+
+if(finalResults % 2 === 0 && whoWin === 'pari'){
+        console.log('utente ha vinto')
+}else if(finalResults % 2 !== 0 && whoWin === 'dispari'){
+        console.log('utente ha vinto')
+}else{
+        console.log('computer ha vinto')
+    }
+
+
+
+
+// creo una funzione per leggere la parola inserita esercizio 1
 
 function isPalindrome(word1, word2){
 
@@ -39,32 +108,6 @@ function isPalindrome(word1, word2){
     // output true o false
 }
 
-console.log(relust)
-
-// --- consegna 2
-// Pari e Dispari
-// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
-// Sommiamo i due numeri
-// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
-// Dichiariamo chi ha vinto.
-
-// chiedo a utente se il risultato della somma è pari o dispari
-let whoWin = prompt('il risultato della somma è pari o dispari?');//string
-console.log('utente ha scelto ' + whoWin);
-
-
-
-// chiesto a utente di inserire numero da 1 a 5 
-let numberUser = parseInt(prompt('inserisci numero da 1 a 5')); //number
-console.log(numberUser); //number
-
-// genero un numero random dal computer 
-let randomNumber = Math.floor(Math.random()* 5) + 1; //number
-console.log(randomNumber); //number
-
-
-
 
 // sommo i due numeri e restituisco 
 function sumNumber(num1, num2){
@@ -76,21 +119,5 @@ function sumNumber(num1, num2){
     // output number
 }
 
-let finalResults = sumNumber(numberUser, randomNumber); // invocazione functio isSumEveen
-console.log('la somma è ' + finalResults)
 
-
-//creo un ciclo che legge chi vince secondo utente
-let chooseEveen = 'pari';
-let chooseOdd = 'dispari'
-
-
-    if(finalResults % 2 === 0 && whoWin === chooseEveen){
-        console.log('utente ha vinto')
-    }else if(finalResults % 2 !== 0 && whoWin === chooseOdd){
-        console.log('utente ha vinto')
-    }else{
-        console.log('computer ha vinto')
-        
-    }
 
